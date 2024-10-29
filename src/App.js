4dbae90049ed7ef1331c0cd2b921a3e8e8d1c603
believer297 <amaraymen5@gmail.com>
2024-10-29 14:22:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Player from "./Player";
+import players from "./players"; // Import your players array
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery">
+      {players.map((player, index) => (
+        <Player
+          key={index}
+          name={player.name}
+          team={player.team}
+          nationality={player.nationality}
+          age={player.age}
+          imageUrl={player.imageUrl}
+          jerseyNumber={player.jerseyNumber}
+        />
+      ))}
     </div>
   );
 }
